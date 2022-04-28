742f6a8249d2435b7a36dd7ccb8cbc197b924008
siwenfeng <siwenfeng@sunshineftg.com>
2022-04-28 16:27:51 +0800

    项目初始化

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+/*
+ * @Description: 
+ * @Autor: siwenfeng
+ * @Date: 2022-04-28 14:17:06
+ * @LastEditors: siwenfeng
+ * @LastEditTime: 2022-04-28 16:27:07
+ */
+// import fs = require('fs')
+// import { Writable } = require('stream');
+// import *  as fs from 'fs'
+// console.log(fs)
