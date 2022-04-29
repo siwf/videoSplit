cce7883b1d8ac0dd8d44cdc47d064b438b6bd3bc
siwenfeng <siwenfeng@sunshineftg.com>
2022-04-29 17:24:16 +0800

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,27 @@
+import * as ffmpegPath from '@ffmpeg-installer/ffmpeg';
+import * as  ffprobePath  from '@ffprobe-installer/ffprobe';
+import * as  ffmpeg from 'fluent-ffmpeg';
+import * as  path from 'path';
 
-import ffmpegPath = require('@ffmpeg-installer/ffmpeg')
-// import ffmpeg = require("fluent-ffmpeg");
-// import path = require("path");
 
-// interface VideoConfig {
-//   filePath: String; // 视频输入路径
-//   startTime: String; // 视频切割开始时间 00:00:00
-//   endTime: String; // 视频切割结束时间 00:00:00
-//   intervalTime: Number; // 时间间隔 单位秒 60
-//   outputPath: String; // 视频输出路径
-// }
+interface VideoConfig {
+  filePath: String; // 视频输入路径
+  startTime: String; // 视频切割开始时间 00:00:00
+  endTime: String; // 视频切割结束时间 00:00:00
+  intervalTime: Number; // 时间间隔 单位秒 60
+  outputPath: String; // 视频输出路径
+}
 
-// const videoCofing: VideoConfig = {
-//   filePath: path.join(__dirname, "../", "public/test.mp4"),
-//   startTime: "00: 00: 00",
-//   endTime: "",
-//   intervalTime: 10 * 60,
-//   outputPath: path.join(__dirname, "../", "output"),
-// };
+const videoCofing: VideoConfig = {
+  filePath: path.join(__dirname, "../", "public/test.mp4"),
+  startTime: "00: 00: 00",
+  endTime: "",
+  intervalTime: 10 * 60,
+  outputPath: path.join(__dirname, "../", "output"),
+};
 // fix bug Cannot find ffmpeg
-console.log(ffmpegPath)
-// ffmpeg.setFfmpegPath(ffmpegPath.path);
-// ffmpeg(videoCofing.filePath)
+ffmpeg.setFfprobePath(ffprobePath.path);
+ffmpeg.setFfmpegPath(ffmpegPath.path);
+
+ffmpeg(videoCofing.filePath)
 console.log('end')
